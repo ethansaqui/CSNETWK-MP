1524222a5b7173906db87102ef028388b70d7131
Sakui <104416692+ethansaqui@users.noreply.github.com>
2022-11-29 21:11:49 +0800

    add: help command functionality

--- client/commands.py
+++ client/commands.py
@@ -6,22 +6,51 @@ def tokenizeCommandString(commandString):
         return commandString[1:].split(' ')
     else:
         print("Invalid command! Start commands with a /")   
+    
+def checkParams(command, numParams, expectedParams):
+    if numParams == expectedParams:
+        return True
+    print(f"Error: Expected {expectedParams} params for command [{command}], but {numParams} were given")
+    return False
 
 def commandSwitch(command):
     action = command[0]
     parameters = command[1:]
     
-    print(action)
-    print(parameters)
-    if command == "join":
+    if action == "join":
+        if(checkParams("join", len(parameters), 2)):
+            join(parameters[0], parameters[1])
+    if action == "leave":
         return
-    if command == "leave":
-        return
-    if command == "msg":
-        return
-    if command == "register":
-        return
-    if command == "all":
-        return
-    if command == "?":
-        return
+    if action == "msg":
+        if(checkParams("msg", len(parameters), 2)):
+            return
+    if action == "register":
+        if(checkParams("register", len(parameters), 1)):
+            return
+    if action == "all":
+        if(checkParams("all", len(parameters), 1)):
+            return
+    if action == "?":
+        commandList()
+
+    
+def join():
+    return
+
+def commandList():
+    print("""+==========================+=============================================+======================+
+|         Command          |                 Description                 |     Sample Usage     |
++==========================+=============================================+======================+
+| /join <server_ip> <port> | Connect to a server with IP and Port        | /join 127.0.0.1 5000 |
++--------------------------+---------------------------------------------+----------------------+
+| /leave                   | Disconnect from the current server          | /disconnect          |
++--------------------------+---------------------------------------------+----------------------+
+| /register <handle>       | Register a unique handle or alias           | /register Milize     |
++--------------------------+---------------------------------------------+----------------------+
+| /all <message>           | Send a message to all users                 | /all I have arrived! |
++--------------------------+---------------------------------------------+----------------------+
+| /msg <handle> <message>  | Send a message to a specific user or handle | /msg Milize G valo?  |
++--------------------------+---------------------------------------------+----------------------+
+| /?                       | Display this menu                           | /?                   |
++--------------------------+---------------------------------------------+----------------------+""")
