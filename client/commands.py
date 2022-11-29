5382725d8bcedc8276f5b1633a8770abb1a6d21a
Sakui <104416692+ethansaqui@users.noreply.github.com>
2022-11-29 20:25:41 +0800

    add: client command parser

--- client/commands.py
+++ client/commands.py
@@ -1,2 +1,12 @@
-def waitForInput():
-    input('Enter a command:\n')
+def parseCommandString(commandString):
+    if(not commandString): return None
+    if(commandString[0] == '/'):
+        word = ''
+        for letter in commandString[1:]:
+            if(letter == ' '):
+                return word
+            word += letter
+        return word
+    else:
+        print("Invalid command! Start commands with a /")
+    
--- client/main.py
+++ client/main.py
@@ -6,6 +6,8 @@ bufferSize = 1024
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-
-
-client.shutdown();
+while(True):
+    commandString = None
+    while commandString == None: 
+        commandString = input('Enter a command:\n')
+    command = commands.parseCommandString(commandString)
